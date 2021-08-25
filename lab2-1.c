e1fe12f66a60427467bb56767f1aa2a528f1bbcd
Thanakan Kanphairor <77429497+TAneKAnz@users.noreply.github.com>
2021-08-25 13:58:11 +0000

    finished 2.1

---
+++ lab2-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  printf("Enter number:");
+  scanf("%d", &n);
+  printf("Factoring Result: ");
+  int i = 2;
+  while (1 < n)
+  {
+    if (n % i == 0)
+    {
+      n = n / i;
+      printf("%d ", i);
+      i = 1;
+      if (i != n)
+      {
+        printf("x ");
+      }
+    }
+    i++;
+  }
+  printf("\n");
+}
